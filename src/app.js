955b24537d2e9b5d7382a6a99beaa8c1e86ce3ed
Dustin S <texnofobix@users.noreply.github.com>
2016-07-02 23:49:53 -0400

    Enable timeline to app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,41 @@
  *
  * By texnofobix (Dustin S.)
  */
+
+var timeline = require('./timeline');
+var api_key = 'SB75i7qh4r3etz5rx4dwl73jm3o41wgf'; //sandbox
+
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
+
 console.log('WHA started!');
+
+//pintest
+  console.log('PebbleKit JS ready!');
+
+  // An hour ahead
+  var date = new Date();
+  date.setHours(date.getHours() + 1);
+
+  // Create the pin
+  var pin = {
+    "id": "pin-" + PIN_ID,
+    "time": date.toISOString(),
+    "layout": {
+      "type": "genericPin",
+      "title": "Example Pin",
+      "body": "This is an example pin from the timeline-push-pin example app!",
+      "tinyIcon": "system://images/SCHEDULED_EVENT"
+    }
+  };
+
+  console.log('Inserting pin in the future: ' + JSON.stringify(pin));
+
+  timeline.insertUserPin(pin, function(responseText) { 
+    console.log('Result: ' + responseText);
+  });
+//endpintest
+
 
 var confVersion = '0.2.0';
 
@@ -139,4 +173,6 @@ function clock() {
   counter = counter + 1;
   console.log('WHA Reload' + counter);
   getstates();
+}
+
 }
